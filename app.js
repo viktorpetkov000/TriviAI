d6f3f97734a524b7a7688cd383ffaa3b9419ed19
viktorpetkov000 <viktor.petkov000@gmail.com>
2023-08-12 17:24:42 +0300

    Changed AJAX requests to Sockets Changed EJS to Pug

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// app.js
-const express = require("express");
+const express = require('express');
 const app = express();
 const https = require('https');
+const socketio = require('socket.io');
+
 let options = {
   protocol: 'https:',
   hostname: 'opentdb.com',
@@ -11,46 +12,41 @@ let options = {
   sslVersion: 'TLSv1.2'
 };
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/js'));
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const server = app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
 
-app.get("/", (req, res) => {
+const io = socketio(server);
+
+app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Add a new route to handle the fetch request from the client
-app.get("/getQuestion", (req, res) => {
-  // Call the getQuestion function and send the result as JSON
-  getQuestion().then((question) => {
-    res.json(question);
-  });
-});
-
-function getCategories() {
-  options.path = '/api_categories.php'
-  const request = https.request(options, (response) => {
-    response.on('data', (data) => {
-      console.log(data.toString());
-    });
-  });
-  request.end();
-}
-
-function getQuestion() {
-  options.path = '/api.php?amount=1&type=multiple'
-  // Return a promise that resolves with the question object
-  return new Promise((resolve, reject) => {
+io.on('connection', socket => {
+  socket.on('getQuestion', () => {
+    options.path = '/api.php?amount=1&type=multiple';
     const request = https.request(options, (response) => {
       response.on('data', (data) => {
         let question = JSON.parse(data.toString());
         console.log(question);
-        resolve(question);
+        socket.emit('question', question);
       });
     });
     request.end();
   });
-}
+
+  socket.on('getCategory', () => {
+    options.path = '/api_category.php';
+    const request = https.request(options, (response) => {
+      response.on('data', (data) => {
+        let categories = JSON.parse(data.toString());
+        console.log(categories);
+        socket.emit('category', categories);
+      });
+    });
+    request.end();
+  });
+});
--- js/index.js
+++ js/index.js
@@ -1,20 +1,30 @@
 $(function() {
-  $("#test-button").on('click', getQuestion);
-  function getQuestion() {
-    $.ajax ({
-      url: "/getQuestion",
-      dataType: "json",
-      success: function (data) {
-        console.log('test');
-        console.log(data);
-        $("#question-container").html(`
-          <p>${data.results[0].question}</p>
-          <ul>
-            <li>${data.results[0].correct_answer}</li>
-            ${data.results[0].incorrect_answers.map((answer) => `<li>${answer}</li>`).join("")}
-          </ul>
-        `);
-      }
-    });
-  }
+  const socket = io();
+  const btnCategory = $('#btn-category');
+  const btnQuestion = $('#btn-question');
+  const categoryDiv = $('#category');
+  const questionDiv = $('#question');
+
+  btnCategory.on('click', () => {
+    socket.emit('getCategory');
+  });
+
+  btnQuestion.on('click', () => {
+    socket.emit('getQuestion');
+  });
+
+  socket.on('category', data => {
+    categoryDiv.html(JSON.stringify(data));
+  });
+
+  socket.on('question', data => {
+    console.log(data);
+    questionDiv.html(`
+      <p>${data.results[0].question}</p>
+      <ul>
+        <li>${data.results[0].correct_answer}</li>
+        ${data.results[0].incorrect_answers.map((answer) => `<li>${answer}</li>`).join("")}
+      </ul>
+    `);
+  });
 });
